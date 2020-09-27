b96e636ac3f25a4ed2e4f7a5942c362b7e29d41a
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-09-26 23:07:30 -0500

    using a .json file to store poses. Using bulma for basic styles.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,77 @@
 import React from "react";
 import "./App.css";
+import poses from "./poses.json";
 
 function App() {
   // array of card suits used as a lookup table.
-  var suits = ["standing", "balance", "seated", "twists"];
+  // var suits = ["standing", "balance", "seated", "twists"];
 
   // holds all possible values that a card can have.
   // var poses = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-  var standPoses = ["warrior I", "warriorII", "high lunge", "triangle"];
-  var balancePoses = ["balanced table", "tree", "warrior III", "plank"];
-  var seatPoses = ["bound eagle", "hero", "pigeon", "prayer squat"];
-  var twistPoses = ["prayer twist", "supine spinal twist", "thread the needle", "belly twist"];
+  // var standPoses = ["warrior I", "warriorII", "high lunge", "triangle"];
+  // var balancePoses = ["balanced table", "tree", "warrior III", "plank"];
+  // var seatPoses = ["bound eagle", "hero", "pigeon", "prayer squat"];
+  // var twistPoses = ["prayer twist", "supine spinal twist", "thread the needle", "belly twist"];
 
-  const getDeck = () => {
-    var deck = new Array();
-    for (var i = 0; i < suits.length; i++){
-      for (var x = 0; x < balancePoses.length; x++){
-        var card = {Value: values[x], Suit: suits[i]};
-        deck.push(card);
-      }
-    }
-    return deck;
-  }
+  // const getDeck = () => {
+  //   var deck = [];
+  //   for (var i = 0; i < suits.length; i++){
+  //     for (var x = 0; x < balancePoses.length; x++){
+  //       var card = {value: values[x], suit: suits[i]};
+  //       deck.push(card);
+  //     }
+  //   }
+  //   return deck;
+  // }
 
-  const shuffle = (deck) => {
-    // for 1000 turns, switch the values of two random cards.
-    for (var i = 0; i < 1000; i++){
-      var location1 = Math.floor((Math.random() * deck.length));
-		  var location2 = Math.floor((Math.random() * deck.length));
-		  var tmp = deck[location1];
+  // const shuffle = (deck) => {
+  //   // for 1000 turns, switch the values of two random cards.
+  //   for (var i = 0; i < 1000; i++){
+  //     var location1 = Math.floor((Math.random() * deck.length));
+  // 	  var location2 = Math.floor((Math.random() * deck.length));
+  // 	  var tmp = deck[location1];
 
-		  deck[location1] = deck[location2];
-		  deck[location2] = tmp;
-    }
-  }
+  // 	  deck[location1] = deck[location2];
+  // 	  deck[location2] = tmp;
+  //   }
+  // }
 
+  const newData = poses.map((pose) => {
+    return (
+      <div key ={pose.id} className="card">
+        <div className="card-image">
+          <figure 
+            //className="image"
+          >
+            <img
+              style={{width: "100px", height: "auto"}}
+              //className="image"
+              src={pose.img_url}
+              // "https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
 
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{pose.english_name}</p>
+            </div>
+          </div>
 
+          <div className="content">
+            Sanskrit name:{pose.sanskrit_name}
+          </div>
+        </div>
+      </div>
+    );
+  });
 
-
-
-
-  return <div className="App"></div>;
+  return <div className="App">
+    {newData}
+  </div>;
 }
 
 export default App;
