43d64be1784060bd20898081b8fa4fec4a760661
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-09-27 21:51:09 -0500

    updated json file to have poses. checkbox console log true or false.

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import poses from "./poses.json";
-import FilterBy from './components/header'
+import FilterBy from "./components/header";
 
 function App() {
-  // array of card suits used as a lookup table.
-  // var suits = ["standing", "balance", "seated", "twists"];
+  const [filteredTypes, setFilteredTypes] = useState([]);
+  
+  // [standing, balanced, seated, twists]
 
-  // holds all possible values that a card can have.
-  // var poses = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  // filteredPoses -> all poses if nothing is checked
 
-  // var standPoses = ["warrior I", "warriorII", "high lunge", "triangle"];
-  // var balancePoses = ["balanced table", "tree", "warrior III", "plank"];
-  // var seatPoses = ["bound eagle", "hero", "pigeon", "prayer squat"];
-  // var twistPoses = ["prayer twist", "supine spinal twist", "thread the needle", "belly twist"];
+  const handleCheck = (poseType, isChecked) => {
+    console.log(poseType);
+    // if checked, add to filteredTypes
+    if (isChecked) {
+      setFilteredTypes([...filteredTypes, poseType]);
+    } else {
+      // if we uncheck a box, we want to filter out that pose
+      setFilteredTypes(filteredTypes.filter(pose => pose.type !== poseType));
+    }
+  };
+  
+  const filteredPoses = poses.filter(pose => {
+    // if standing pose checked, return those 
+    // if [].contains(pose.type)
+    return pose;
+  });
 
-  // const getDeck = () => {
-  //   var deck = [];
-  //   for (var i = 0; i < suits.length; i++){
-  //     for (var x = 0; x < balancePoses.length; x++){
-  //       var card = {value: values[x], suit: suits[i]};
-  //       deck.push(card);
-  //     }
-  //   }
-  //   return deck;
-  // }
 
-  // const shuffle = (deck) => {
-  //   // for 1000 turns, switch the values of two random cards.
-  //   for (var i = 0; i < 1000; i++){
-  //     var location1 = Math.floor((Math.random() * deck.length));
-  // 	  var location2 = Math.floor((Math.random() * deck.length));
-  // 	  var tmp = deck[location1];
-
-  // 	  deck[location1] = deck[location2];
-  // 	  deck[location2] = tmp;
-  //   }
-  // }
-
-  const newData = poses.map((pose) => {
+  const newData = filteredPoses.map((pose) => {
     return (
-      <div key ={pose.id} className="card">
-        <div className="card-image">
-          <figure 
-            //className="image"
-          >
-            <img
-              style={{width: "100px", height: "auto"}}
-              //className="image"
-              src={pose.img_url}
-              // "https://versions.bulma.io/0.7.1/images/placeholders/1280x960.png"
-              alt="Placeholder image"
-            />
-          </figure>
+      <div className="card" key={pose.id}>
+        <div className="poses">
+          <img src={pose.img_url} alt="Placeholder image"/>
         </div>
 
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">{pose.english_name}</p>
-            </div>
+        <div>
+          <div>
+            <p className="title is-4">{pose.english_name}</p>
           </div>
 
-          <div className="content">
-            Sanskrit name:{pose.sanskrit_name}
-          </div>
+          <div>Sanskrit name:{pose.sanskrit_name}</div>
         </div>
       </div>
     );
   });
 
-  return <div className="App">
-    <FilterBy />
-    {newData}
-  </div>;
+  return (
+    <div className="App">
+      <FilterBy handleCheck={handleCheck} />
+      {newData}
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import lotus from '../lotus.svg';
-// import "./App.css";
-// import poses from "./poses.json";
+import React, { useState } from "react";
 
-const FilterBy = () => {
-    return(
-        <div>
-            <h2>Yoga Poses Library</h2>
+const FilterBy = (props) => {
 
+  return (
+    <div>
+      <div>
+        <h2>Yoga Poses Library</h2>
+        <img id="lotus" src="/images/lotus.svg" />
+      </div>
 
-            <p>Filter Poses: </p>
+      <p>Filter Poses: </p>
 
-            <p>
-            <label htmlFor="standing">Standing</label>
-            <input type="checkbox" id="standing" />
-            </p>
+      <p>
+        <label htmlFor="standing">Standing</label>
+        <input type="checkbox" id="standing" onChange={(e) => props.handleCheck("standing", e.target.checked)}/>
+      </p>
 
-            <p>
-            <label htmlFor="standing">Balanced</label>
-            <input type="checkbox" id="balanced" />
-            </p>
+      <p>
+        <label htmlFor="standing">Balanced</label>
+        <input type="checkbox" id="balanced" />
+      </p>
 
-            <p>
-            <label htmlFor="standing">Seated</label>
-            <input type="checkbox" id="seated" />
-            </p>
+      <p>
+        <label htmlFor="standing">Seated</label>
+        <input type="checkbox" id="seated" />
+      </p>
 
-            <p>
-            <label htmlFor="standing">Twists</label>
-            <input type="checkbox" id="twists" />
-            </p>
-        </div>
-    )
-}
+      <p>
+        <label htmlFor="standing">Twists</label>
+        <input type="checkbox" id="twists" />
+      </p>
+    </div>
+  );
+};
 
 export default FilterBy;
